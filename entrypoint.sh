35c40cf1b077de3a0f69dfac042bd66ef51172f0
Brian Hopkins <brianhh1230@gmail.com>
2017-11-23 09:12:54 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-python3 -m spacy download en
-python3 -m rasa_core.train -s data/stories.md -d domain.yml -o models/dialogue
 python3 -m rasa_core.server -d data/models/dialogue -u models/nlu/current
